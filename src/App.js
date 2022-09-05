4e1c8a762a3f5bb3e900255a41be09362dfc622e
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2022-09-05 12:11:30 +0300

    added Footer

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './App.css';
 import Header from './components/Header/Header';
 import Products from './components/Navbar/Products';
 import Main from './components/Main/Main';
-import PerformanceType from './components/Navbar/PerformanceType';
+import ExecutionTypes from './components/Navbar/ExecutionTypes/ExecutionTypes';
 import Applying, { AllApplication } from './components/Navbar/Applying/Applying';
 import Objects from './components/Navbar/Objects';
 import Articles from './components/Navbar/Articles';
 import Contacts from './components/Navbar/Contacts';
 import ApplyTypeComponent from './components/Navbar/Applying/ApplyTypeComponent';
-import NotFounded from './components/NotFounded';
+import NotFoundedPage from './components/NotFoundedPage/NotFoundedPage';
+import Footer from './components/Footer/Footer';
 
 function App(props) {
   return ( 
     <>
+      <div className='container App'>
         <Header/>
-        <div className='container App'>
         <Routes>
-        <Route path = '/' element={<Main/>}/>
+          <Route path = '/' element={<Main/>}/>
           <Route path = 'products' element={<Products/>}/>
-          <Route path = 'performanceType' element={<PerformanceType/>}/>
+          <Route path = 'executionTypes' element={<ExecutionTypes/>}>
+  
+          </Route>
           <Route path = 'applying' element={<Applying/>}>
             <Route path ='' element={<AllApplication applicationTypes = {props.state.applicationTypes}/>}/>
             <Route path =':applyType' 
@@ -28,10 +31,10 @@ function App(props) {
           <Route path = 'objects' element={<Objects/>}/>
           <Route path = 'articles' element={<Articles/>}/>
           <Route path = 'contacts' element={<Contacts/>}/>
-          <Route path = '*' element={<NotFounded/>}/>
+          <Route path = '*' element={<NotFoundedPage/>}/>
         </Routes>
-        </div>
-        
+        <Footer/>
+      </div>
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <div className='container bgGrey colorGrey pb-5' >
+          <div className='row py-5'>
+            <div className='col ps-5'>
+              <div className='col-5'>
+                <a href='/'>
+                  <img 
+                    src='https://thumb.tildacdn.com/tild6638-3066-4333-a637-663961376662/-/resize/140x/-/format/webp/__.png'
+                    className='w-100'
+                  />
+                </a>
+              </div>
+              <div style={{fontSize:'12px'}}>
+                <div>ООО ГК «Буровые технологии»</div>
+                <div>ОГРН: 1167847080565</div>
+                <div>ИНН: 7816318009</div>
+                <div>© Все права защищены, копирование без разрешения владельца запрещено</div>
+              </div>
+            </div>
+            <div className='col pt-1'>
+              <div>
+                <NavLink className='text-decoration-none text-muted fw-bold' to='/'>
+                   ГЛАВНАЯ СТРАНИЦА
+                </NavLink>
+              </div>
+              <div className='pt-3'>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Продукция</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Объекты</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted'to=''>Статьи</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted'to=''>Контакты</NavLink></div>
+              </div>
+            </div>
+            <div className='col pt-1'>
+              <div>
+                <NavLink className='text-decoration-none text-muted fw-bold' to='/'>ПРИМЕНЕНИЕ</NavLink>
+              </div>
+              <div className='pt-3'>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для загородного дома</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для бани</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для забора</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для пирса и причала</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для производственных зданий</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для гаража</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для беседки</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для фонарных столбов</NavLink></div>
+              </div>
+            </div>
+            <div className='col pt-1'>
+              <div>
+                <NavLink className='text-decoration-none text-muted fw-bold' to='/'>ВИДЫ ИСПОЛНЕНИЯ</NavLink>
+              </div>
+              <div className='pt-3'>
+                <div><NavLink className='text-decoration-none text-muted' to=''>С ленточным ростверком</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>С обвязкой из бруса</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted'to=''>С обвязкой швеллером</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для участков с уклоном</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>С установкой анкеров</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>С выпуском арматуры</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для мачт освещения</NavLink></div>
+                <div><NavLink className='text-decoration-none text-muted' to=''>Для террасы или крыльца</NavLink></div>
+              </div>
+            </div>
+          </div> 
+        </div>
+      </footer>
+    </div>
+  )
+}
