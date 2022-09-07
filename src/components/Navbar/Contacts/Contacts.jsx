e0db4eb0d9ede9a1512a2ee08948aed71927ec53
ZenkoWu <anna.zagrebaylo.03@mail.ru>
2022-09-07 15:49:46 +0300

    add content to Contacts

--- src/components/Navbar/Contacts/Contacts.jsx
+++ src/components/Navbar/Contacts/Contacts.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import GetCatalog from '../../Main/GetCatalog';
+import AllArticles from '../Articles/AllArticles';
 import Breadcrumbs from './../../Breadcrumbs/Breadcrumbs';
+import { Navigate } from 'react-router-dom';
 
-export default function Contacts() {
+export default function Contacts({articles}) {
   return (
     <div>
       <Breadcrumbs currentPage = 'Контакты'/>
-      Contacts
+      <h2 className='text-center fw-bold'>Контакты компании «СВАЯ групп»</h2>
+      <div className='py-5'>
+        <div className='col-5 border p-4'>
+          <h4>Контакты</h4>
+          <h6 className='py-3'>8 812 500-05-54</h6>
+          <h6 className='py-3'>info@svayagrup.ru</h6>
+          <h6 className='py-3'>Санкт-Петербург, Южное шоссе, дом 37, корпус 4</h6>
+        </div>
+       </div>
+       <h2 className='text-center fw-bold'>Наши выполненные объекты</h2>
+       {/* <AllArticles articles={articles}/> */}
+       <GetCatalog/>
     </div>
   )
 }
